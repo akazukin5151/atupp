1bee93917a1a60c697dce96908ddddc69ac91b14
akazukin5151 <tsuiyikching@protonmail.com>
2022-10-07 16:13:43 +0100

    Improve appearance of plot_props

--- python/plot_props.py
+++ python/plot_props.py
@@ -3,15 +3,20 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 london = pd.read_csv('data/london_props.csv')
-london['city'] = 'london'
+london['City'] = 'London'
 
 tokyo = pd.read_csv('data/tokyo_props.csv')
-tokyo['city'] = 'tokyo'
+tokyo['City'] = 'Tokyo'
 
 df = pd.concat([london, tokyo])
 
-g = sns.barplot(data=df, x='max_dist', y='prop', hue='city')
+_, g = plt.subplots(figsize=(15, 9))
+sns.barplot(data=df, x='max_dist', y='prop', hue='City', ax=g)
 
+sns.despine()
 g.set_xticklabels(g.get_xticklabels(), rotation=90)
+g.set_xlabel('Distance')
+g.set_ylabel('Proportion of population within distance of a station')
+
 plt.tight_layout()
 plt.savefig('out/props.png')
