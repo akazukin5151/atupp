1a9c990583a139558c6f1d2176b868ecf1af12ab
akazukin5151 <tsuiyikching@protonmail.com>
2022-10-07 14:50:47 +0100

    Plot facet grid of all quadrants separately in plot_quadrants_map.py

--- python/plot_quadrants_map.py
+++ python/plot_quadrants_map.py
@@ -4,17 +4,36 @@ import pandas as pd
 import seaborn as sns
 
 def main(ax, city, point_type):
-    reds = pd.read_csv(f'data/{city}_{point_type}.csv')
+    reds = pd.read_csv(f'data/{city}_{point_type}s.csv')
     reds = reds.sample(1000)
     reds['x'] = reds['x'].astype(float)
     reds['y'] = reds['y'].astype(float)
-
-    sns.scatterplot(data=reds, x='x', y='y', ax=ax, color=point_type.removesuffix('s'))
+    reds['point_type'] = point_type
+    return reds
 
 
 _, ax = plt.subplots()
 city = sys.argv[1]
-for point_type in {'reds', 'oranges', 'blues', 'greens'}:
-    main(ax, city, point_type)
+
+grays = pd.read_csv(f'data/{city}_pp_meters.csv')
+grays = grays.sample(1000)
+grays['x'] = grays['x'].astype(float)
+grays['y'] = grays['y'].astype(float)
+
+df = None
+
+for point_type in {'red', 'orange', 'blue', 'green'}:
+    color_df = main(ax, city, point_type)
+    if df is None:
+        df = color_df
+    else:
+        df = pd.concat([df, color_df])
+
+g = sns.FacetGrid(data=df, col='point_type')
+for col_val, ax in g.axes_dict.items():
+    sns.scatterplot(ax=ax, data=grays, x='x', y='y', color='gray')
+    sns.scatterplot(
+        ax=ax, data=df[df['point_type'] == col_val], x='x', y='y', color=col_val
+    )
 
 plt.savefig(f'out/{city}_quadrants_map.png')
